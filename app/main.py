26e3e833dafb9b24ddffd8d1bcb6e6327579c966
Juan Miguel De Moya Candanoza <jmigueldemoya@unibarranquilla.edu.co>
2026-07-21 15:11:56 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
+        "https://pet-centric-front.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
